2e798e1d323c777c2c755bdcbd7630317267fada
d-laslo <ice_bear@ukr.net>
2024-09-15 09:31:10 +0300

    added resize with the mouse

--- widget/widget.py
+++ widget/widget.py
@@ -4,9 +4,12 @@ from PyQt6.QtGui import QMouseEvent
 from .properties import Properties
 
 class DesktopWidget(QWidget):
-    __need_drag_widget = True
+    __allow_drag_widget = True
+    __drag_position = None
+    __resizing = False
+    __mouse_pos = 0
 
-    def __init__(self, properties:Properties):
+    def __init__(self, properties:Properties, x = 1, y = 1):
         super().__init__()
 
         self.setWindowFlags(Qt.WindowType.FramelessWindowHint | Qt.WindowType.WindowStaysOnTopHint)
@@ -21,37 +24,77 @@ class DesktopWidget(QWidget):
         
         self.__properties = properties
         self.__properties.init_widget_template(self)
+        self.setGeometry(x,y,*properties.get_start_size())
+        self.adjustSize()
+        self.__set_resize_mod(properties.is_resizable)
 
         self.__timer = QTimer(self)
         self.__timer.timeout.connect(self.__update_time)
         self.__timer.start(1000)
-        
-        self.setGeometry(1,1,*properties.get_start_size())
-        self.drag_position = None
+    
+    @property      
+    def __allow_resize(self):
+        return not (self.minimumSize() == self.maximumSize())
         
     def __update_time(self):
         self.__properties.callback()
 
-    def set_drag_widget(self, need_drag_widget:bool) -> None:
-        self.__need_drag_widget = need_drag_widget
+    def __set_drag_widget(self, allow_drag_widget:bool) -> None:
+        self.__allow_drag_widget = allow_drag_widget
+        
+    def __set_resize_mod(self, allow_resize:bool) -> None:
+        if allow_resize:
+            self.setMinimumSize(50, 50)
+            self.setMaximumSize(16777215, 16777215)
+        else:
+            self.setFixedSize(self.width(), self.height())
 
-    def mousePressEvent(self, event:QMouseEvent) -> None:
-        if self.__need_drag_widget and event.button() == Qt.MouseButton.LeftButton:
-            self.drag_position = event.globalPosition().toPoint() - self.frameGeometry().topLeft()
-            event.accept()
+    def __is_near_edge(self, pos):
+        margin = 10
+        return pos.x() > self.width() - margin and pos.y() > self.height() - margin
+
+    def mousePressEvent(self, event:QMouseEvent) -> None:            
+        if event.button() == Qt.MouseButton.LeftButton:
+            self.__mouse_pos = event.globalPosition().toPoint()
+            if self.__is_near_edge(event.pos()):
+                self.__resizing = True
+            elif self.__allow_drag_widget:
+                self.__drag_position = event.globalPosition().toPoint() - self.frameGeometry().topLeft()
             
         if event.button() == Qt.MouseButton.MiddleButton:
-            self.set_drag_widget(not self.__need_drag_widget)
+            self.__set_drag_widget(not self.__allow_drag_widget)
+            self.__set_resize_mod(not self.__allow_resize and self.__properties.is_resizable)
             
         if event.button() == Qt.MouseButton.RightButton:
             self.__properties.settings_window(self)
+        
+        event.accept()
 
     def mouseMoveEvent(self, event:QMouseEvent) -> None:
-        if self.__need_drag_widget and event.buttons() == Qt.MouseButton.LeftButton and self.drag_position is not None:
-            self.move(event.globalPosition().toPoint() - self.drag_position)
-            event.accept()
+        if self.__allow_drag_widget and event.buttons() == Qt.MouseButton.LeftButton and self.__drag_position is not None:
+            self.move(event.globalPosition().toPoint() - self.__drag_position)
+            
+        if self.__resizing:
+            diff = event.globalPosition().toPoint() - self.__mouse_pos
+            self.resize(
+                max(self.width() + diff.x(), self.minimumWidth()), 
+                max(self.height() + diff.y(), self.minimumHeight())
+            )
+            self.__mouse_pos = event.globalPosition().toPoint()
+        else:
+            if self.__is_near_edge(event.pos()):
+                self.setCursor(Qt.CursorShape.SizeFDiagCursor)
+            else:
+                self.setCursor(Qt.CursorShape.ArrowCursor)
+        event.accept()
 
     def mouseReleaseEvent(self, event:QMouseEvent) -> None:
-        if self.__need_drag_widget and event.button() == Qt.MouseButton.LeftButton:
-            self.drag_position = None
-            event.accept()
+        self.__resizing = False
+        if self.__allow_drag_widget and event.button() == Qt.MouseButton.LeftButton:
+            self.__drag_position = None
+        event.accept()
+    
+    def resizeEvent(self, event):
+        if not self.__allow_resize:
+            return
+        super().resizeEvent(event)
