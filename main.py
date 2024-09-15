d3dabc8846069154325a03c39b9109539120c16b
d-laslo <ice_bear@ukr.net>
2024-09-15 18:26:32 +0300

    some performance update

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ from PyQt6.QtWidgets import QApplication
     
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    
     w = DesktopWidget(Clock())
     w.show()
     
     w2 = DesktopWidget(CpuLoadGraph())
     w2.show()
+    
     sys.exit(app.exec())
--- widget/widgets/cpu_load_graph/cpu_load_graph.py
+++ widget/widgets/cpu_load_graph/cpu_load_graph.py
@@ -6,7 +6,6 @@ import psutil
 import matplotlib
 matplotlib.use('agg')
 import matplotlib.pyplot as plt
-import mplcyberpunk 
 from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg as FigureCanvas
 from threading import Thread
 
@@ -25,26 +24,31 @@ class CustomFigureCanvas(FigureCanvas):
         self.parent.mouseMoveEvent(event)
 
 class CpuLoadGraph(Properties):
+    __is_started = True
+
     def __init__(self, width=200, heigh=100, frequency_update = 1000):
         self.__data = [0] * 100
-        self.__figure, self.__ax = plt.subplots()
         self.__width = width
         self.__heigh = heigh
-        self.__is_started = True
         self.__frequency_update = frequency_update
-
-        self.__figure.set_size_inches(*self.__pixels_to_inches(self.__width, self.__heigh, self.__figure.dpi), True)
+        
+        self.__figure, self.__ax = plt.subplots()    
+        self.__ax.set_facecolor((0.18, 0.18, 0.18, 0.1))
+        self.__figure.patch.set_facecolor((0.11, 0.11, 0.11, 0.1))
+        self.__figure.subplots_adjust(left=0, right=1, top=1, bottom=0)
+        self.__figure.set_size_inches(*self.__pixels_to_inches(self.__width, self.__heigh, self.__figure.dpi))
+        
         Thread(target=self.__update_cpu_data, daemon=True).start()
     
     def callback(self):
-        self.update_graph()
+        self.__update_graph()
     
     def init_widget_template(self, widget:DesktopWidget):
         self.__canvas = CustomFigureCanvas(self.__figure, widget)
         layout = QVBoxLayout()
         layout.addWidget(self.__canvas)
         widget.setLayout(layout)
-        self.update_graph()
+        self.__create_graph()
     
     def settings_window(self, widget:DesktopWidget):
         pass
@@ -62,25 +66,22 @@ class CpuLoadGraph(Properties):
     @property
     def frequency_update(self):
         return self.__frequency_update
-
-    def update_graph(self):
-        self.__ax.clear()
-        plt.style.use("cyberpunk")        
-        self.__ax.set_facecolor((0.18, 0.18, 0.18, 0.1))
-        self.__figure.patch.set_facecolor((0.11, 0.11, 0.11, 0.1))
-
-        self.__ax.plot(self.__data, color='cyan', linewidth=2)
-        self.__ax.fill_between(range(len(self.__data)), self.__data, color='cyan', alpha=0.3)
+    
+    def __create_graph(self):
+        (self.__line,) = self.__ax.plot(self.__data, color='cyan', linewidth=2)
 
         self.__ax.set_ylim(0, 100)
         self.__ax.set_xlim(0, len(self.__data) - 1)
         
         self.__ax.set_xticks([])
         self.__ax.set_yticks([])
-
-        mplcyberpunk.add_glow_effects()
-        self.__figure.subplots_adjust(left=0, right=1, top=1, bottom=0)
+        
         self.__canvas.draw()
+
+    def __update_graph(self):
+        self.__line.set_ydata(self.__data)
+        self.__canvas.draw()
+        self.__canvas.flush_events()
         
     def __update_cpu_data(self):
         while self.__is_started:
